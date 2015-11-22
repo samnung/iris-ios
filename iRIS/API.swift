a10529e4dfd5fe886becb86c39fd34da824252e3
Roman Kříž <samnung@gmail.com>
2015-11-22 17:54:01 +0100

    [API] Fix parsing of DataPlus items

--- iRIS/API.swift
+++ iRIS/API.swift
@@ -14,7 +14,8 @@ enum ServerResult<T> {
 }
 
 enum ServerError: ErrorType {
-    case InvalidKeyInJson(String)
+    case MissingKeyInJson(String, Any)
+    case InvalidTypeOfKeyInJson(String, Any)
     case ParsingJson
 }
 
@@ -23,53 +24,72 @@ typealias JSONObject = [String: AnyObject]
 
 extension Dictionary where Key: StringLiteralConvertible {
     func get<T>(key: Key) throws -> T {
-        if let value = self[key] as? T {
-            return value
-        } else {
-            throw ServerError.InvalidKeyInJson(key as! String)
+        guard let rawValue = self[key] else {
+            throw ServerError.MissingKeyInJson(key as! String, self)
         }
+        
+        guard let value = rawValue as? T else {
+            throw ServerError.InvalidTypeOfKeyInJson(key as! String, self)
+        }
+        
+        return value
+    }
+    
+    func getOptional<T>(key: Key) -> T? {
+        guard let rawValue = self[key] else {
+            return nil
+        }
+        
+        guard let value = rawValue as? T else {
+            return nil
+        }
+        
+        return value
     }
 }
 
 
 struct DataItem {
     var bear: Float = 0.0
-    var course: String = ""
+    var course: String? = ""
     var delay: Int = 0
     var endStop: Int = 0
-    var endStopX: String = ""
+    var endStopX: String? = ""
     var LF: Bool = true
     var lastStop: Int = 0
     var lat: Double = 0
     var line: Int = 0
     var lng: Double = 0
-    var operatorId: Int = 0
+    var operatorId: Int? = 0
     var route: Int = 0
     var startStop: Int = 0
     
     init(id: String, data: JSONObject) throws {
-        let bearString: String = try data.get("Bear")
-        bear = Float(bearString)!
-        course = try data.get("Course")
+        let bearString: String? = data.getOptional("Bear")
+        let bearInt: Int? = data.getOptional("Bear")
+        bear = bearString.flatMap { Float($0) } ?? bearInt.flatMap { Float($0) } ?? 0.0
+        
+        course = data.getOptional("Course")
         delay = try data.get("Delay")
         endStop = try data.get("EndStop")
-        endStopX = try data.get("EndStopX")
+        endStopX = data.getOptional("EndStopX")
         LF = try data.get("LF")
         lastStop = try data.get("LastStop")
         lat = try data.get("Lat")
         line = try data.get("Line")
         lng = try data.get("Lng")
-        operatorId = try data.get("Operator")
+        operatorId = data.getOptional("Operator")
         route = try data.get("Route")
         startStop = try data.get("StartStop")
     }
 }
 
 func downloadAllData(completion: (ServerResult<[DataItem]>) -> ()) {
-    let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+    let session = NSURLSession.sharedSession()
 
     let url = NSURL(string: "http://iris.bmhd.cz/api/data.json")!
     let request = NSMutableURLRequest(URL: url)
+    request.HTTPMethod = "GET"
     request.addValue("http://iris.bmhd.cz/", forHTTPHeaderField: "Referer")
 
     let task = session.dataTaskWithRequest(request) { data, response, error in
@@ -86,7 +106,16 @@ func downloadAllData(completion: (ServerResult<[DataItem]>) -> ()) {
                     }
                 }
                 
-                completion(.Ok(items))
+                let innerData2 = json["DataPlus"] as! JSONObject
+                for (key, value) in innerData2 {
+                    if let parsed = try? DataItem(id: key, data: value as! JSONObject) {
+                        items.append(parsed)
+                    }
+                }
+                
+                let newItems = items.filter { $0.line == 702 }
+                
+                completion(.Ok(newItems))
             } else {
                 completion(.Error(ServerError.ParsingJson))
             }
